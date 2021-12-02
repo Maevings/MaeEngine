a8d205116bef1a488f33d5e1764bd1ab67badb68
Jackson <68143771+Maevings@users.noreply.github.com>
2021-12-02 08:11:24 -0600

    wow you suc

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -63,16 +63,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['virgin', 0.2], //From 0% to 19%
+		['boo you suck', 0.4], //From 20% to 39%
+		['no', 0.5], //From 40% to 49%
+		['a', 0.6], //From 50% to 59%
+		['oh ok', 0.69], //From 60% to 68%
+		['funny number', 0.7], //69%
+		['papayas', 0.8], //From 70% to 79%
+		['yes', 0.9], //From 80% to 89%
+		['the w', 1], //From 90% to 99%
+		['chad', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	#if (haxe >= "4.0.0")
